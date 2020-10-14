841b599a36d27216672f4488b5527613f9273405
Septs <github@septs.pw>
2020-10-14 22:03:48 +0800

    add sms-tpdu bridge

---
+++ sms/bridge/deliver.go
@@ -0,0 +1,31 @@
+package bridge
+
+import (
+	"bytes"
+
+	"github.com/NiceLabs/go-smpp/pdu"
+	"github.com/NiceLabs/go-smpp/sms"
+)
+
+func ToDeliverSM(deliver *sms.Deliver) (sm *pdu.DeliverSM, err error) {
+	var message pdu.ShortMessage
+	if deliver.Flags.UDHIndicator {
+		message.UDHeader = pdu.UserDataHeader{}
+	}
+	_, err = message.ReadFrom(bytes.NewReader(deliver.UserData))
+	sm = &pdu.DeliverSM{
+		SourceAddr: pdu.Address{
+			TON: deliver.OriginatingAddress.TON,
+			NPI: deliver.OriginatingAddress.NPI,
+			No:  deliver.OriginatingAddress.No,
+		},
+		ESMClass: pdu.ESMClass{
+			MessageType:  deliver.Flags.MessageType.Type(),
+			UDHIndicator: deliver.Flags.UDHIndicator,
+			ReplyPath:    deliver.Flags.ReplyPath,
+		},
+		ProtocolID: deliver.ProtocolIdentifier,
+		Message:    message,
+	}
+	return
+}
---
+++ sms/bridge/submit.go
@@ -0,0 +1,29 @@
+package bridge
+
+import (
+	"bytes"
+
+	"github.com/NiceLabs/go-smpp/pdu"
+	"github.com/NiceLabs/go-smpp/sms"
+)
+
+func ToSubmit(sm *pdu.SubmitSM) (submit *sms.Submit, err error) {
+	var userData bytes.Buffer
+	_, _ = sm.Message.UDHeader.WriteTo(&userData)
+	userData.Write(sm.Message.Message)
+	submit = &sms.Submit{
+		Flags: sms.SubmitFlags{
+			ReplyPath:               sm.ESMClass.ReplyPath,
+			UserDataHeaderIndicator: sm.Message.UDHeader != nil,
+		},
+		DestinationAddress: sms.Address{
+			NPI: sm.DestAddr.NPI,
+			TON: sm.DestAddr.TON,
+			No:  sm.DestAddr.No,
+		},
+		ProtocolIdentifier: sm.ProtocolID,
+		DataCoding:         byte(sm.Message.DataCoding),
+		UserData:           userData.Bytes(),
+	}
+	return
+}
